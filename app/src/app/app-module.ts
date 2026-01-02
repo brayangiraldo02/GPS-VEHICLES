1b0e0e5ae0205efe2d4cd1a57fca5e30a1b1fc74
Brayan Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2026-01-02 16:43:28 -0500

    feat(front): add angular material and new core and shared modules

--- app/src/app/app-module.ts
+++ app/src/app/app-module.ts
@@ -2,7 +2,7 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
+import { App } from './app.component';
 
 @NgModule({
   declarations: [
