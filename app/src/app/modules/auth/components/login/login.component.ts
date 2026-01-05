444798f3cd4b9f150a2c601ea452d82532f86c0f
Brayan Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2026-01-05 16:57:03 -0500

    feat(front): new login component

--- app/src/app/modules/auth/components/login/login.component.ts
+++ app/src/app/modules/auth/components/login/login.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  logoURL =
+    'https://media.licdn.com/dms/image/C4E0BAQFN9RPjAJAIdA/company-logo_200_200/0/1631324563752?e=2147483647&v=beta&t=wArf89ExUKjZh0xk-BOPfnLhlvSw5F-fHyf4wC8uK4Y';
 }
