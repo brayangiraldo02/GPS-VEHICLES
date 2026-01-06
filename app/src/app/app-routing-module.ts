2742100af364f0502df7b090c4cd319eed19d8e9
Brayan Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2026-01-06 15:58:44 -0500

    feat(front): creation of all modules

--- app/src/app/app-routing-module.ts
+++ app/src/app/app-routing-module.ts
@@ -12,20 +12,51 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./modules/dashboard/dashboard-module').then((m) => m.DashboardModule),
+      },
+      {
+        path: 'tablas',
+        loadChildren: () =>
+          import('./modules/tables/tables-module').then((m) => m.TablesModule),
+      },
+      {
+        path: 'inventarios',
+        loadChildren: () =>
+          import('./modules/inventory/inventory-module').then((m) => m.InventoryModule),
+      },
+      {
+        path: 'facturacion',
+        loadChildren: () =>
+          import('./modules/billing/billing-module').then((m) => m.BillingModule),
+      },
+      {
+        path: 'cartera',
+        loadChildren: () =>
+          import('./modules/portfolio/portfolio-module').then((m) => m.PortfolioModule),
+      },
+      {
+        path: 'inspecciones',
+        loadChildren: () =>
+          import('./modules/inspections/inspections-module').then((m) => m.InspectionsModule),
+      },
+      {
+        path: 'utilidades',
+        loadChildren: () =>
+          import('./modules/utilities/utilities-module').then((m) => m.UtilitiesModule),
+      },
+      {
         path: '',
         redirectTo: 'dashboard',
         pathMatch: 'full',
       },
-      {
-        path: 'dashboard',
-        loadChildren: () => import('./modules/dashboard/dashboard-module').then((m) => m.DashboardModule),
-      }
-    ]
+    ],
   },
   {
     path: '**',
     redirectTo: 'login',
-  }
+  },
 ];
 
 @NgModule({
