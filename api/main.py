6aa440744c2bb126a6a0afd96b93ae6dfdb780c5
xbedoya <ximena.bedoya@utp.edu.co>
2026-01-06 15:43:10 -0500

    feat(back): implement user login

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from config.dbconnection import Base, engine
 from dotenv import load_dotenv
+from routes.users import users_router
 import os
 
 load_dotenv()
@@ -9,6 +10,8 @@ app = FastAPI()
 # app = FastAPI(root_path="/api", docs_url=None, redoc_url=None, openapi_url=None)
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(users_router)
 
 @app.get("/")
 def main():
