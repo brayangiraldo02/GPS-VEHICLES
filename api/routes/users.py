5556d7805b481abbeda98f7ca65405beade0f260
xbedoya <ximena.bedoya@utp.edu.co>
2026-04-14 10:12:11 -0500

    fix

--- api/routes/users.py
+++ api/routes/users.py
@@ -6,6 +6,6 @@ from controller.users import user_info
 
 users_router = APIRouter()
 
-@users_router.get('/info', tags=["Users"])
+@users_router.get('/info/', tags=["Users"])
 async def get_user_info(db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
   return await user_info(db, current_user)
