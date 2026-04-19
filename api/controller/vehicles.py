4d389a6433ca8b4c38926b082f4293350554280f
xbedoya <ximena.bedoya@utp.edu.co>
2026-04-19 14:01:11 -0500

    feat(back-vehicles): modify vehicles_per_owner endpoint response

--- api/controller/vehicles.py
+++ api/controller/vehicles.py
@@ -13,25 +13,13 @@ from models.estados import Estados
 async def vehicles_per_owner(owner_id: str, db: Session):
   try:
     query = db.query(
+      Vehiculos.ID,
       Vehiculos.PLACA,
       Marcas.NOMBRE.label('Brand'),
       Vehiculos.MODELO,
-      Colores.NOMBRE.label('Color'),
-      TiposVehiculos.NOMBRE.label('Vehicle_type'),
       Vehiculos.ID_PROPIE.label('Owner_id'),
       Propietarios.NOMBRE.label('Owner_name'),
-      Vehiculos.SERVICIO,
-      Estados.NOMBRE.label('Status'),
-      Vehiculos.CUO_ADMON,
-      Vehiculos.IVA,
-      Vehiculos.PREND_APAG,
-      Vehiculos.GPS_SERIAL,
-      Vehiculos.CEL_SERIAL,
-      Vehiculos.CEL_NUMERO.label('Cel_num'),
-      Vehiculos.FEC_CREADO
     ).outerjoin(Marcas, Vehiculos.ID_MARCA == Marcas.ID)\
-     .outerjoin(Colores, Vehiculos.ID_COLOR == Colores.ID)\
-     .outerjoin(TiposVehiculos, Vehiculos.ID_TIPOVEH == TiposVehiculos.ID)\
      .outerjoin(Propietarios, Vehiculos.ID_PROPIE == Propietarios.ID)\
      .outerjoin(Estados, Vehiculos.ID_ESTADO == Estados.ID)
 
@@ -45,22 +33,11 @@ async def vehicles_per_owner(owner_id: str, db: Session):
          
     response = [
       {
+        'id': vehicle.ID,
         'plate': vehicle.PLACA, 
         'brand': vehicle.Brand,
         'model': vehicle.MODELO,
-        'color': vehicle.Color,
-        'vehicle_type': vehicle.Vehicle_type,
-        'owner_id': vehicle.Owner_id,
         'owner_name': vehicle.Owner_name,
-        'service': vehicle.SERVICIO,
-        'status': vehicle.Status,
-        'cuo_admon': vehicle.CUO_ADMON,
-        'iva': vehicle.IVA,
-        'prend_apag': vehicle.PREND_APAG,
-        'gps_serial': vehicle.GPS_SERIAL,
-        'cel_serial': vehicle.CEL_SERIAL,
-        'cel_num': vehicle.Cel_num,
-        'date_created': vehicle.FEC_CREADO
       } for vehicle in vehicles
     ]
     return JSONResponse(content=jsonable_encoder(response), status_code=200)
--- api/routes/vehicles.py
+++ api/routes/vehicles.py
@@ -5,6 +5,6 @@ from controller.vehicles import vehicles_per_owner
 
 vehicles_router = APIRouter()
 
-@vehicles_router.get('/vehicles-per-owner/{owner_id}/', tags=["Vehicles"])
-async def get_vehicles(owner_id: str = None, db: Session = Depends(get_db)):
+@vehicles_router.post('/vehicles-per-owner/', tags=["Vehicles"])
+async def post_vehicles(owner_id: str = None, db: Session = Depends(get_db)):
   return await vehicles_per_owner(owner_id, db)
