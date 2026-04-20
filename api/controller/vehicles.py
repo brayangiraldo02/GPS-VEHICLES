fa0bdcaeaacb8f3e3c84d266da81f0152b521816
xbedoya <ximena.bedoya@utp.edu.co>
2026-04-20 10:00:50 -0500

    feat(back-vehicles): add vehicle info retrieval endpoint

--- api/controller/vehicles.py
+++ api/controller/vehicles.py
@@ -43,3 +43,59 @@ async def vehicles_per_owner(owner_id: str, db: Session):
     return JSONResponse(content=jsonable_encoder(response), status_code=200)
   except Exception as e:
     return JSONResponse(content={"error": str(e)}, status_code=500)
+  
+# ---------------------------------------------------------------------------------------------------------------
+
+async def vehicle_info(vehicle_plate: str, db: Session):
+  try:
+    vehicle = db.query(
+      Vehiculos.ID,
+      Vehiculos.PLACA,
+      Marcas.NOMBRE.label('Brand'),
+      Vehiculos.MODELO,
+      Colores.NOMBRE.label('Color'),
+      TiposVehiculos.NOMBRE.label('Vehicle_type'),
+      Vehiculos.ID_PROPIE.label('Owner_id'),
+      Propietarios.NOMBRE.label('Owner_name'),
+      Vehiculos.SERVICIO,
+      Estados.NOMBRE.label('Status'),
+      Vehiculos.CUO_ADMON,
+      Vehiculos.IVA,
+      Vehiculos.PREND_APAG,
+      Vehiculos.GPS_SERIAL,
+      Vehiculos.CEL_SERIAL,
+      Vehiculos.CEL_NUMERO,
+      Vehiculos.FEC_CREADO
+    ).outerjoin(Marcas, Vehiculos.ID_MARCA == Marcas.ID)\
+     .outerjoin(Colores, Vehiculos.ID_COLOR == Colores.ID)\
+     .outerjoin(TiposVehiculos, Vehiculos.ID_TIPOVEH == TiposVehiculos.ID)\
+     .outerjoin(Propietarios, Vehiculos.ID_PROPIE == Propietarios.ID)\
+     .outerjoin(Estados, Vehiculos.ID_ESTADO == Estados.ID)\
+     .filter(Vehiculos.PLACA == vehicle_plate).first()
+
+    if not vehicle:
+      return JSONResponse(content={"message": "No vehicle found"}, status_code=404)
+         
+    response = {
+      'id': vehicle.ID,
+      'plate': vehicle.PLACA, 
+      'brand': vehicle.Brand,
+      'model': vehicle.MODELO,
+      'color': vehicle.Color,
+      'vehicle_type': vehicle.Vehicle_type,
+      'owner_id': vehicle.Owner_id,
+      'owner_name': vehicle.Owner_name,
+      'service': vehicle.SERVICIO,
+      'status': vehicle.Status,
+      'cuo_admon': vehicle.CUO_ADMON,
+      'iva': vehicle.IVA,
+      'prend_apag': vehicle.PREND_APAG,
+      'gps_serial': vehicle.GPS_SERIAL,
+      'cel_serial': vehicle.CEL_SERIAL,
+      'cel_num': vehicle.CEL_NUMERO,
+      'date_created': vehicle.FEC_CREADO
+    }
+
+    return JSONResponse(content=jsonable_encoder(response), status_code=200)
+  except Exception as e:
+    return JSONResponse(content={"error": str(e)}, status_code=500)
--- api/routes/vehicles.py
+++ api/routes/vehicles.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from config.dbconnection import get_db
-from controller.vehicles import vehicles_per_owner
+from controller.vehicles import *
 
 vehicles_router = APIRouter()
 
 @vehicles_router.post('/vehicles-per-owner/', tags=["Vehicles"])
 async def post_vehicles(owner_id: str = None, db: Session = Depends(get_db)):
   return await vehicles_per_owner(owner_id, db)
+
+@vehicles_router.post('/info/', tags=["Vehicles"])
+async def post_vehicle_info(vehicle_plate: str, db: Session = Depends(get_db)):
+  return await vehicle_info(vehicle_plate, db)
